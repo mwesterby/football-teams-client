b03e796473f3c4a087597f9cbb16e3db4f7d2ac3
Michael Westerby <michael.westerby@ibm.com>
2020-09-27 12:29:17 +0100

    Added Sorting

--- src/components/clubTable.js
+++ src/components/clubTable.js
@@ -9,10 +9,25 @@ class ClubTable extends React.Component {
       }
     }
   
+    sortClubs(clubs) {
+        clubs.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+              if(nameA < nameB) {
+                  return -1;
+              }
+              if(nameA > nameB) {
+                  return 1;
+              }
+              return 0;
+          });
+        return clubs;
+    }
+
     async componentDidMount() {
-      const clubs = await apiRequests.getClubs();
+      let clubs = await apiRequests.getClubs();
       this.setState({
-        clubs: clubs
+        clubs: this.sortClubs(clubs)
       });
     }
   
